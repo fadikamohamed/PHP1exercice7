6ec9a2bba0eab9ce58a667ae40d17bfbbadd697d
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:44:16 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 7</title>
+  </head>
+  <body>
+    <p>
+    <?php
+      $lastname = 'FADIKA';
+      $firstname = 'Mohamed';
+      $age = 30;
+      echo 'Bonjour ' . $lastname . ' ' . $firstname . ' tu as ' . $age . ' ans.';
+     ?>
+   </p>
+  </body>
+</html>
